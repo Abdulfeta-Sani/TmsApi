d9ca4faf92017dc2db43cd53e1945a6be71814e2
Abdulfeta-Sani <abdulfetasani128@gmail.com>
2026-07-16 07:42:17 +0300

    feat(database): configure Certificate entity mapping

---
+++ Configurations/CertificateConfiguration.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TmsApi.Entities;
+
+namespace TmsApi.Data.Configurations;
+
+public class CertificateConfiguration : IEntityTypeConfiguration<Certificate>
+{
+    public void Configure(EntityTypeBuilder<Certificate> builder)
+    {
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.SerialNumber)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.HasIndex(c => c.SerialNumber)
+            .IsUnique();
+
+        builder.Property(c => c.IssuedAt)
+            .IsRequired();
+
+        builder.HasOne(c => c.Student)
+            .WithMany(s => s.Certificates)
+            .HasForeignKey(c => c.StudentId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(c => c.Course)
+            .WithMany(c => c.Certificates)
+            .HasForeignKey(c => c.CourseId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
