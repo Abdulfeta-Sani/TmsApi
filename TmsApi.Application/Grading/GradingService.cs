878b1d7817f3c26cbf3c8f84e0f112c79d406ff9
Abdulfeta-Sani <abdulfetasani128@gmail.com>
2026-09-05 15:23:26 +0300

    test: add grading service unit tests

---
+++ TmsApi.Application/Grading/GradingService.cs
@@ -0,0 +1,35 @@
+namespace TmsApi.Application.Grading;
+
+public class GradingService
+{
+    public const decimal DistinctionThreshold = 70m;
+    public const decimal PassThreshold = 50m;
+
+    public GradeLevel CalculateLetterGrade(decimal score, decimal maxScore)
+    {
+        if (maxScore <= 0m || score < 0m || score > maxScore)
+        {
+            return GradeLevel.Invalid;
+        }
+
+        var percentage = score / maxScore * 100m;
+
+        return percentage >= DistinctionThreshold
+            ? GradeLevel.Distinction
+            : percentage >= PassThreshold
+                ? GradeLevel.Pass
+                : GradeLevel.Fail;
+    }
+
+    public GradeLevel CalculateFromEnrollmentGrade(decimal? enrollmentGradePercent)
+    {
+        if (enrollmentGradePercent is null)
+        {
+            return GradeLevel.Invalid;
+        }
+
+        return CalculateLetterGrade(
+            enrollmentGradePercent.Value,
+            maxScore: 100m);
+    }
+}
